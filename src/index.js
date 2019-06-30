81dcf21956ccb0013788687e6c18d8d52d0a8888
TylerOlli <ollityler@gmail.com>
2019-06-29 22:04:57 -0400

    Created first React component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, welcome to my project!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React with Webpack and Babel!';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
