6dad296ebc183a1e9918d4a0be66c5684a667e92
TylerOlli <ollityler@gmail.com>
2019-06-29 22:09:58 -0400

    Created function component and passsed props to it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 const title = 'React with Webpack and Babel!';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App title={title} />,
   document.getElementById('app')
 );
 
